6e44f95993a3803c1ab87e80eefed0068074beee
Carlos Augusto Borges <calimaborges@gmail.com>
2023-11-15 10:55:22 -0300

    feature(aoc): 2015 day 1 part 2

--- 2015/day1/main.c
+++ 2015/day1/main.c
@@ -1,18 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define UP '('
 #define DOWN ')'
+#define BASEMENT -1
 
 int main() {
+	int current_position = 1;
 	int current_floor = 0;
-	for (char c = getchar(); c != EOF; c = getchar()) {
+	for (char c = getchar(); c != EOF; c = getchar(), current_position++) {
 		if (c == UP) {
 			current_floor++;
 		} else if (c == DOWN) {
 			current_floor--;
 		}
+
+		if (current_floor == BASEMENT) {
+			break;
+		}
 	}
 
-	printf("%d\n", current_floor);
+	printf("current floor: %d\n", current_floor);
+	printf("current position: %d\n", current_position);
 	return 0;
 }
