4459eb249e797714dcdca1f7003155c9a6a51f72
Carlos Augusto Borges <calimaborges@gmail.com>
2023-12-13 13:33:23 -0300

    feature(aoc): 2015 day 5 part 2

--- 2015/day5/main.c
+++ 2015/day5/main.c
@@ -1,62 +1,43 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdbool.h>
 #include <string.h>
 
 #define MAX_INPUT 20
 
-// it contains at least 3 vowels
-// contains at least one letter that appears twice in a row
-// it does not contain 'ab', 'cd, 'pq', 'xy'
-
-int contains_any(char* input, char* sequence[2], int sequence_size) {
-	for (unsigned int i = 0; i < sequence_size; ++i) {
-		if (strstr(input, sequence[i]) != NULL) {
-			return 1;
-		}
-	}
-	return 0;
-}
-
-int count_chars(char* input, char char_array[], int char_array_size) {
-	int counter = 0;
-	for (unsigned int i = 0; input[i] != '\0' || i > MAX_INPUT; ++i) {
-		for (unsigned int j = 0; j < char_array_size; ++j) {
-			if (input[i] == char_array[j]) {
-				counter++;
+bool contains_non_overlapping_pair_of_any_two_letters(char *input) {
+	int len = strnlen(input, MAX_INPUT);
+	for (int i = 0; i < len - 1; i++) {
+		for (int j = i + 2; j < len - 1; j++) {
+			if (input[i] == input[j] && input[i + 1] == input[j + 1]) {
+				return true;
 			}
 		}
 	}
-	return counter;
+	return false;
 }
 
-int contains_duplicate(char *input) {
-	char last_char = input[0];
-	for (unsigned int i = 1; input[i] != '\0' || i > MAX_INPUT; ++i) {
-		if (input[i] == last_char) {
-			return 1;
+bool repeats_with_letter_in_between(char *input) {
+	int len = strnlen(input, MAX_INPUT);
+	for(unsigned int i = 2; i < len; ++i) {
+		if (input[i - 2] == input[i]) {
+			return true;
 		}
-		last_char = input[i];
 	}
-	return 0;
+	return false;
 }
 
 
-int is_nice_string(char* input) {
-	char* forbidden[] = { "ab", "cd", "pq", "xy" };
-	size_t forbidden_size = sizeof(forbidden) / sizeof(char*);
-	if (contains_any(input, forbidden, forbidden_size)) {
-		return 0;
+bool is_nice_string(char* input) {
+	if (!contains_non_overlapping_pair_of_any_two_letters(input)) {
+		printf("%s failed pair check\n", input);
+		return false;
 	}
-	char vowels[] = { 'a', 'e', 'i', 'o', 'u' };
-	size_t vowels_size = sizeof(vowels) / sizeof(char);
-	if (count_chars(input, vowels, vowels_size) < 3) {
-		return 0;
+	if (!repeats_with_letter_in_between(input)) {
+		printf("%s failed repeat check\n", input);
+		return false;
 	}
-
-	if (!contains_duplicate(input)) {
-		return 0;
-	}
-	return 1;
+	printf("%s success\n", input);
+	return true;
 }
 
 
@@ -64,9 +45,7 @@ int main() {
 	char input[MAX_INPUT];
 	int counter = 0;
 	while (scanf("%19s", input) != EOF) {
-		int check = is_nice_string(input);
-		printf("%s %d\n", input, check);
-		if (check) {
+		if (is_nice_string(input)) {
 			counter++;
 		}
 	}
