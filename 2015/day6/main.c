85cfb96382148807647c89aeb384aa088657ec32
Carlos Augusto Borges <calimaborges@gmail.com>
2023-12-14 13:14:20 -0300

    feature(aoc): 2015 day 6 part 1 -- temporary

---
+++ 2015/day6/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+#define MAX_INPUT 100
+
+int main() { 
+	char input[MAX_INPUT];
+
+	// read command
+	// read command modifier (if needed)
+	// read start interval
+	// read through keyword
+	// read end interval
+
+
+	while (scanf("%99s", input) != EOF) {
+		printf("%s\n", input);
+	}
+	return 0;
+}
