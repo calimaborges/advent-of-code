9588ad968066cd567826d15a259a4aad1dd2206e
Carlos Augusto Borges <calimaborges@gmail.com>
2024-12-03 08:09:31 -0300

    feat(python): 2024 day 2 part 2

--- 2024/day2/main.py
+++ 2024/day2/main.py
@@ -21,7 +21,6 @@ def is_interval_safe(level1, level2, previous_interval_type):
             return IntervalType.Unsafe
     return interval_type
 
-
 def is_levels_safe(levels):
     interval_type = None
     for current, next in list(zip(levels, levels[1:])):
@@ -30,6 +29,14 @@ def is_levels_safe(levels):
             return False
     return True
 
+def is_levels_with_bad_level_safe(levels):
+    for i in range(0, len(levels)):
+        partial_levels = levels.copy()
+        del partial_levels[i]
+        if is_levels_safe(partial_levels):
+            return True
+    return False
+
 # Tests
 # print(is_levels_safe([7,6,4,2,1]))
 # print(is_levels_safe([1,2,7,8,9]))
@@ -37,7 +44,17 @@ def is_levels_safe(levels):
 # print(is_levels_safe([8,6,4,4,1]))
 # print(is_levels_safe([1,3,6,7,9]))
 
+
+# Tests
+# print(is_levels_with_bad_level_safe([7,6,4,2,1]))
+# print(is_levels_with_bad_level_safe([1,2,7,8,9]))
+# print(is_levels_with_bad_level_safe([9,7,6,2,1]))
+# print(is_levels_with_bad_level_safe([1,3,2,4,5]))
+# print(is_levels_with_bad_level_safe([8,6,4,4,1]))
+# print(is_levels_with_bad_level_safe([1,3,6,7,9]))
+
 total_safe_levels = 0
+total_safe_levels_bad_level = 0
 with open(input_path, 'r') as file:
     for line in file:
         levels = list(map(int, line.split()))
@@ -45,4 +62,9 @@ with open(input_path, 'r') as file:
         if is_levels_safe(levels):
             total_safe_levels = total_safe_levels + 1
 
+        if is_levels_with_bad_level_safe(levels):
+            total_safe_levels_bad_level = total_safe_levels_bad_level + 1
+
+
 print(total_safe_levels)
+print(total_safe_levels_bad_level)
